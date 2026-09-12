c6beea17a6589cb27f0536e6ccf9e7cff939d5bb
FMCompanion <timmysmith3297@gmail.com>
2026-09-12 15:08:56 +0100

    Add Career Planner package exports

---
+++ src/fmcompanion_career_planner/__init__.py
@@ -0,0 +1,5 @@
+from .planner import build_career_plan, rank_club_candidates
+from .service import create_career_plan
+from .ai_feedback import build_ai_messages, get_ai_feedback
+
+__all__ = ["build_career_plan", "rank_club_candidates", "create_career_plan", "build_ai_messages", "get_ai_feedback"]
